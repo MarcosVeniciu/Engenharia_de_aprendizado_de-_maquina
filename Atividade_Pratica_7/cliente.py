b1cafd0bacf736d4a4c47e9e96bc498d6fcbec97
Marcos Veniciu de sa Barbalho <marcos.barbalho@ufv.br>
2024-05-06 03:16:48 -0300

    Atividade Pratica 7

---
+++ Atividade_Pratica_7/cliente.py
@@ -0,0 +1,37 @@
+from sklearn.metrics import balanced_accuracy_score
+from sklearn.metrics import classification_report 
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics import accuracy_score
+import pandas as pd
+import numpy as np
+import Pyro5.api
+import json
+
+
+def main():
+    # Conecte-se ao servidor remoto
+    uri = Pyro5.api.locate_ns().lookup("servidor")
+    proxy = Pyro5.api.Proxy(uri)
+
+    # O dataset no csv ja esta processado
+    input_data = pd.read_csv("df_teste.csv")
+    input_data = input_data.drop("Unnamed: 0", axis=1)
+    
+    X = input_data.drop("Exited", axis=1)  # Remove da conjunto de teino o atributo Exited, pois ele sera usado para classificar os dados
+    y = input_data["Exited"]
+
+    json_data = X.to_json(orient='records')
+    # Envie os dados para o servidor e receba a classificação
+    prediction = proxy.classificar(json_data)
+    prediction = json.loads(prediction)
+    actual_labels = np.array(y)
+
+    print('Accuracy: ', round(float(accuracy_score(actual_labels, prediction))*100, 4), '%')
+    print('Accuracy Balanced: ', round(float(balanced_accuracy_score(actual_labels, prediction))*100, 4), '%')
+    print('\nClassification Stats:')
+    print(classification_report(actual_labels, prediction))
+
+
+if __name__ == "__main__":
+    main()
---
+++ Atividade_Pratica_7/servidor.py
@@ -0,0 +1,45 @@
+from io import StringIO
+from joblib import load
+import pandas as pd
+import Pyro5.api
+import json
+
+
+
+@Pyro5.api.expose
+class ClassificationServer:
+
+    def __init__(self):
+        self.model = load('modelo_salvo.joblib')
+        print("Modelo Funcionando!!")
+
+
+    @Pyro5.api.expose
+    def classificar(self, input_data):
+        try:
+            json_data = StringIO(input_data)
+            df = pd.read_json(json_data)
+            # Use the model to make a prediction on the input data
+            prediction = self.model.predict(df)
+            # Return the prediction result
+            return json.dumps(prediction.tolist())
+        except Exception as e:
+            return f"Erro ao processar a entrada: {str(e)}"
+
+if __name__ == '__main__':
+    # Inicializa o servidor Pyro5
+    daemon = Pyro5.api.Daemon()
+
+    # Registra o objeto remoto no servidor Pyro5
+    objeto_remoto = ClassificationServer()
+    uri = daemon.register(objeto_remoto)
+
+    # Obtém uma referência ao Name Server
+    ns = Pyro5.api.locate_ns()
+
+    # Registra a URI do objeto remoto no Name Server
+    ns.register("servidor", uri) # registra o name server como servidor
+
+    # Inicia o servidor Pyro5
+    print("Servidor aguardando conexões...")
+    daemon.requestLoop()
